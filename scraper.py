97706bba396ba3b15138dfbc8e25a15c6ccbc587
Rens Dimmendaal <rens@dimmendaal.com>
2022-11-03 22:21:59 +0100

    simplify

--- scraper.py
+++ scraper.py
@@ -2,24 +2,21 @@ from urllib.request import urlopen
 import json
 from pathlib import Path
 
-def get_closed_prs(username):
-    with urlopen(f"https://api.github.com/search/issues?q=state%3Aclosed+type%3Apr+author%3A{username}") as response:
+your_organization = "godatadriven"  # github.com/<your_organization>
+
+fpath = Path("pullrequests.jsonl")
+ids = [json.loads(line)['id'] for line in fpath.read_text().splitlines()]
+
+with urlopen(f"https://api.github.com/orgs/{your_organization}/members") as response:
+    org_members = json.loads(response.read())
+
+for member in org_members:
+    with urlopen(f"https://api.github.com/search/issues?q=state%3Aclosed+type%3Apr+author%3A{member['login']}") as response:
         user_body = json.loads(response.read())
-    return user_body.get("items")
+    closed_prs =  user_body.get("items")
 
-if __name__ == "__main__":
-    your_organization = "godatadriven"
-
-    fpath = Path("pullrequests.jsonl")
-    ids = [json.loads(line)['id'] for line in fpath.read_text().splitlines()]
-    
-    with urlopen(f"https://api.github.com/orgs/{your_organization}/members") as response:
-        org_members = json.loads(response.read())
-
-    for member in org_members:
-        closed_prs = get_closed_prs(member['login'])
-        for pr in closed_prs:
-            if (pr['pull_request']['merged_at'] is not None) and pr['id'] not in ids:
-                with fpath.open('a') as fp:
-                    json.dump(pr,fp)
-                    fp.write("\n")
+    for pr in closed_prs:
+        if (pr['pull_request']['merged_at'] is not None) and pr['id'] not in ids:
+            with fpath.open('a') as fp:
+                json.dump(pr,fp)
+                fp.write("\n")
